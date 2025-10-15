883fef94894ecead2ccc5c2e6876497f129056bb
Abidh007 <abidhforpg@gmail.com>
2025-10-15 11:40:16 +0530

    Add files via upload

---
+++ SUM.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,SUM;
+clrscr();
+printf("enter two numbers");
+scanf("%d%d",&num1,&num2);
+SUM=num1+num2;
+printf("SUM=%d",SUM);
+getch();
+}
+
---
+++ doubly.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+struct node
+{
+    int data
+    
+}
